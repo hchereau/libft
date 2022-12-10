8eb5a98afa4d83f6e74c176bd05fe81041335f7e
oui <hchereau42@gmail.com>
2022-12-10 03:16:02 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -10,12 +10,81 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	**ft_split(const char *s, char c)
+#include "libft.h"
+
+static size_t get_next_sep(char *s, c)
 {
 	
+}
+static char *get_next_words(char **s,char c)
+{
+	size_t	i;
+	size_t	start;
+	char	*word;
+	
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		if(s[i] != c)
+		{
+			start = i;
+			i = get_next_sep(s + start, c);
+			word = (char *)
+			ft_strlcpy(word, s + start, i - start);
+			break;
+		}
+	}
+	return (word);
+}
+
+static size_t	get_size(const char *s,char c)
+{
+	size_t	i;
+	size_t	size_split;
+	bool	is_sep;
+	
+	
+	i = 0;
+	size_split = 1;
+	is_sep = false;
+	while (i < ft_strlen(s))
+	{
+		if (s[i] == c && is_sep == false)
+		{
+			++size_split;
+			is_sep = true;
+		}
+		else if( s[i] != c) 
+			is_sep = false;
+		++i;
+	}
+	return (size_split);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	const size_t	size_split = get_size(s, c);
+	size_t			i;
+	char			**split;
+
+	split = (char **)malloc((size_split + 1) * sizeof(char *));
+	if (split != NULL)
+	{	
+		i = 0;
+		while(i < size_split)
+		{
+			split[i] = get_next_word(&s, c);
+			++i;
+		}
+		split[i] = NULL;
+	}
+	return (split); 
 }
 
 /*
 process:
-	->
-*/
+-> parcourir chaine
+-> separe la chaine quand il y a un separateur
+-> add le mocreau dnas un tableau de char**
+-> return char **
+ */
--- test/srcs/utils.test.c
+++ test/srcs/utils.test.c
@@ -67,25 +67,15 @@ void	check_string_eq(const char *a, const char *b, const size_t n)
 		printf("(mine) [%s] : (expected) [%s] \n", a, b);			
 	}
 
-void	check_string_string_eq(const char **a, const char **b, const size_t n)
+void	check_string_array_eq(const char **a, const char **b, const size_t n)
 {
 	size_t i;
 
 	i = 0;
-	while (*a[i] != NULL && *b[i] != NULL)
-	{
-		if (strcmp(*a[i], *b[i]) != 0)
-		{	
-			printf("%zu : %sKO%s\n", n, RED, WHITE);
-			printf("(mine) [%s] : (expected) [%s] \n", a, b);
-			return (0);			
-		}
-		if ((*a[i + 1] == NULL && *b[i + 1] != NULL) || (*a[i + 1] != NULL && *b[i + 1] == NULL))
-		{	
-			printf("%zu : %sKO%s\n", n, RED, WHITE);
-			return (0);			
-		}
-	}	
-	printf("%zu : %sOK%s\n", n, GREEN, WHITE);
-	return (0);
+	while (a[i] != NULL && b[i] != NULL && strcmp(a[i], b[i]) == 0)
+		++i;
+	if (a[i] == NULL && b[i] == NULL)
+		printf("%zu : %sOK%s\n", n, GREEN, WHITE);
+	else
+		printf("%zu : %sKO%s\n", n, RED, WHITE);
 }
